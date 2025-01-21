a2c86a6e56c962e116ee16c4ceee4b07467d3373
JuanRT <juan.m13@hotmail.fr>
2025-01-21 19:44:14 +0100

    Added foodProductsServices related to the foodProduct endpoints + imported foodProductsModule within app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,21 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { typeorm } from "../config/dataSource";
 import { CarbonEmissionFactorsModule } from "./carbonEmissionFactor/carbonEmissionFactors.module";
+import { FoodProductsModule } from "./foodProduct/foodProducts.module";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [typeorm],
-    }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) =>
-        configService.getOrThrow("typeorm"),
-    }),
-    CarbonEmissionFactorsModule,
-  ],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            load: [typeorm],
+        }),
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) =>
+                configService.getOrThrow("typeorm"),
+        }),
+        CarbonEmissionFactorsModule,
+        FoodProductsModule,
+    ],
 })
 export class AppModule {}
